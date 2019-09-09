4a3d09963e79e35910dd7ea1a8da62b0d61d012a
ebisLab <ebaiden@techie.com>
2019-09-09 19:42:33 -0400

    mvp completed

--- api/actionsRouter.js
+++ api/actionsRouter.js
@@ -5,28 +5,30 @@ const router = express.Router();
 // router.use(express.json());
 
 
-// router.get('/:project_id', (req, res) => {
-//     const { project_id } = req.params;
+router.get('/:project_id', (req, res) => {
+    const { project_id } = req.params;
 
-//     Project.getProjectActions(project_id)
-//         .then(item => {
-//             res.status(200).json(item)
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json({ error: 'error getting projecr' })
-//         })
-//     console.log('user list called');
-// });
+    Project.getProjectActions(project_id)
+        .then(item => {
+            res.status(200).json(item)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: 'error getting projecr' })
+        })
+    console.log('user list called');
+});
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    Action.get(id).then(result => {
-        res.status(201).json({ result })
-    })
+router.get('/', (req, res) => {
+    // const { id } = req.params;
+
+    Action.get()
+        .then(result => {
+            res.status(201).json(result)
+        })
 })
 
-router.post('/:id', (req, res) => {
+router.post('/', validateProjId, (req, res) => {
     const body = req.body
     Action.insert(body).then(action => {
         res.status(201).json({ action })
@@ -36,49 +38,27 @@ router.post('/:id', (req, res) => {
         })
 })
 
-// router.get('/:project_id/actions/:id', validateProjId, validateActionId, (req, res) => {
 
-//     console.log('project called')
-
-//     res.status(200).json(req.action);
-//     // res.status(200).json({ message: "Hello world" });
-// });
-
-// router.post('/', validateProjId, (req, res) => {
-//     // const { body } = req;
-//     // const { id } = req.params
-
-//     console.log('Action created')
-//     Action.insert(req.body)
-//         .then(act => {
-//             res.status(200).json(act);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json({ error: 'Couldnt create the action' });
-
-//         })
-// })
-
-router.put('/:project_id/actions/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     const { body } = req;
     const { id } = req.params
 
     console.log('Action created')
     Action.update(id, body)
         .then(newInfo => {
-            if (newInfo) {
-                console.log('newInfo');
-                Action.get(id)
-                    .then(info => {
-                        res.status(200).json(info)
-                        console.log('info')
-                    })
-                    .catch(err => {
-                        console.log(err)
-                        res.status(500).json({ error: "couldnt update project" });
-                    })
-            }
+            res.status(202).json(newInfo)
+            // if (newInfo) {
+            //     console.log('newInfo');
+            //     Action.get(id)
+            //         .then(info => {
+            //             res.status(200).json(info)
+            //             console.log('info')
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "couldnt update project" });
+            // })
+            // }
         })
         .catch(err => {
             console.log(err);
@@ -88,11 +68,11 @@ router.put('/:project_id/actions/:id', (req, res) => {
 
 })
 
-router.delete('/:project_id/actions/:id', validateActionId, validateProjId, (req, res) => {
+router.delete('/:id', validateActionId, (req, res) => {
     console.log('action has been removed')
     //     // res.status(200).json(user)
     const { id } = req.params;
-    User.remove(id)
+    Action.remove(id)
         .then(() => res.status(204).end())
         .catch(err => {
             console.log(err);
@@ -114,21 +94,6 @@ function validateProjId(req, res, next) {
     } else {
         next();
     }
-
-    // Action.get(project_id)
-    //     .then(specificProj => {
-    //         if (specificProj) {
-    //             console.log('ValidateProject', specificProj)
-    //             res.status(200).json(specificProj);
-    //         } else {
-    //             return res.status(404).json({ error: 'no such id exists' });
-    //         }
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //         res.status(500).json({ error: 'error occurred while retrieving project' });
-    //     })
-
 }
 
 function validateActionId(req, res, next) {
@@ -156,15 +121,4 @@ function validateActionId(req, res, next) {
 
 }
 
-function validaAction(req, res, next) {
-    const { body } = req;
-    const { project_id } = req.params;
-
-    if (project_id != body.project_id) {
-        return res.status(400).json({ error: 'Error in this field' })
-    }
-}
-
 module.exports = router;
-
-
